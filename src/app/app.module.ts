dd2de58615e5de38a6c1e75446735c9a8d9c95ec
SHENKUTE YEGEZU Eskinder <eskinder.shenkute@soprabanking.com>
2017-12-26 19:53:52 +0100

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { CardGamesComponent } from './card-games/card-games.component';
+import { HomeComponent } from './home/home.component';
+import { AuthService } from './auth.service';
+import { UserService } from './user.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardGamesComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'card-games', component: CardGamesComponent }
+    ])
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { AuthService } from './../auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  constructor(private auth: AuthService) { 
+  }
+
+  login() { 
+    this.auth.login();
+  }
+
+}
