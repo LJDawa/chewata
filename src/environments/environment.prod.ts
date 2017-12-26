1577f813212753558b0fc1400905beabecc301b5
SHENKUTE YEGEZU Eskinder <eskinder.shenkute@soprabanking.com>
2017-12-26 16:57:39 +0100

    Initial setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBgW78jDM7BHAkr8jUWubKCgWJ98gW8nL8",
+    authDomain: "chewata27.firebaseapp.com",
+    databaseURL: "https://chewata27.firebaseio.com",
+    projectId: "chewata27",
+    storageBucket: "",
+    messagingSenderId: "353223685039"
+  }
 };
